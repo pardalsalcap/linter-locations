fd9555ce80e748b6be168c2d342c17d897684476
pardalsalcap <dani.casasnovas@gmail.com>
2023-11-24 00:58:38 +0100

    Added comments on the config options

--- config/linter-locations.php
+++ config/linter-locations.php
@@ -2,10 +2,12 @@
 
 // config for Pardalsalcap/LinterLocations
 return [
+    // Locales that will be used during the database population
     'available_locales' => [
         'ca' => 'Català',
         'es' => 'Castellano',
         'en' => 'English',
     ],
+    // If set to true it will modify the default filament search to search into the json fields
     'use_scoped_search' => true,
 ];
