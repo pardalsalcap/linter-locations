75162551c8d24b5de4cbcd6b5cbeaefcb127f072
pardalsalcap <dani.casasnovas@gmail.com>
2023-11-24 00:59:09 +0100

    Added translations to english

---
+++ resources/lang/en/countries.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'navigation' => 'Countries',
+    'model_label' => 'Country',
+    'model_label_plural' => 'Countries',
+    'iso_field' => 'ISO Code',
+    'name_field' => 'Name',
+    'translations_field' => 'Translation in :l',
+    'iso_column' => 'ISO Code',
+    'name_column' => 'Name',
+    'continent_id_column' => 'Continent',
+    'continent_id_field' => 'Continent',
+    'navigation_group' => 'Locations',
+];
---
+++ resources/lang/en/states.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'navigation' => 'Provinces',
+    'model_label' => 'Province',
+    'model_label_plural' => 'Provinces',
+    'iso_field' => 'Code',
+    'name_field' => 'Name',
+    'translations_field' => 'Translation in :l',
+    'iso_column' => 'Code',
+    'name_column' => 'Name',
+    'country_id_column' => 'Country',
+    'country_id_field' => 'Country',
+    'community_id_field' => 'Autonomous Community',
+    'community_id_column' => 'Autonomous Community',
+    'navigation_group' => 'Locations',
+];
